3cca790e71fb0b83702e23c3deb3a5c03839a6b0
Sumandeep2111 <sumandeep21111994@gmail.com>
2020-01-31 19:07:18 -0500

    set home location

--- app/src/main/java/com/example/overlyademo/MapsActivity.java
+++ app/src/main/java/com/example/overlyademo/MapsActivity.java
@@ -1,8 +1,13 @@
 package com.example.overlyademo;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -12,6 +17,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -78,5 +84,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             }
         };
+        if(!checkPermission())
+            requestPermision();
+        else
+            getLocation();
+    }
+    @SuppressLint("MissingPermission")
+    private void getLocation(){
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,0,locationListener);
+        Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        //set the home location as home location
+        setHomeLocation(lastKnownLocation);
+    }
+
+    private  void requestPermision(){
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+
+    }
+
+    private boolean checkPermission(){
+        int permissionStatus = ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionStatus == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (REQUEST_CODE == requestCode) {
+            if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,10,locationListener);
+            }
+        }
+    }
+    private void setHomeLocation(Location location){
+       mMap.clear();
+       LatLng userlocation = new LatLng(location.getLatitude(),location.getLongitude());
+       MarkerOptions options = new MarkerOptions().position(userlocation).title("My Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).snippet("you are here");
+       homeMarker = mMap.addMarker(options);
+       mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userlocation,15));
     }
 }
